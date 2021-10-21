c94598d9ebb2d9e1f34f34b1651d6c176ac9ee14
ChristopherSmyt <>
2021-10-21 11:58:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").toggle();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
